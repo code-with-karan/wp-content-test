266473a14da29aec5c6ea87a39d7d8e662e40624
code-with-karan <karannarvani39@gmail.com>
2020-05-04 21:26:43 +0530

    submit test

---
+++ themes/codetest/index.php
@@ -0,0 +1,126 @@
+<?php get_header(); ?>
+<body>
+		<div class="row slide">
+			<h1 align="center" class="Ar">Architecture<br/> And Decore.</h1>
+			<img class="pos" src="<?php bloginfo('template_url'); ?>/images/main-menu-icon-png-3.png" height="50px">
+		</div>
+		<div class="container second">
+			<div class="row firstc">
+				<h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry. </h1>
+			</div>
+		</div>	
+		<div class="container">
+			<div class="row mysp" >
+				<div class="col-md-6 col">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.
+					</p>
+				</div>
+				<div class="col-md-6 col">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry. 
+					</p>
+				</div>	
+			</div>				
+		</div>	
+		<div class="container">
+			<div class="row third">
+				<div class="col-md-6 ">
+					 <img src="<?php bloginfo('template_url'); ?>/images/download.jpg" height=600px width="500px" class="image container2">
+					   <div class="overlay">BuyNOw</div>
+				</div>
+				<div class="col-md-6 container2">
+					<img src="<?php bloginfo('template_url'); ?>/images/pexels-photo-1396122.jpeg"  ><br/>
+					<a href="#" class="link1">Go To About</a>				
+				</div>	
+			</div>
+		</div>
+		<div class="container">
+			<div class="row four">
+				<h1>Lorem Ipsum</h1>
+				<div class="row">	
+					<div class="col-md-6 con2">
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting 
+						</p>
+					</div>
+				</div>
+			</div>		
+		</div>
+		<div class="container">
+			<div class="row">
+				<img src="<?php bloginfo('template_url'); ?>/images/living-2-2-1080x675.jpg" height="600" width="100%">
+			</div>
+		</div>
+		<div class="container">
+			<div class="row four">
+				<h1>Lorem Ipsum</h1>
+				<div class="row">	
+					<div class="col-md-6 con2">
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.typesetting industry.Ipsum is simply dummy text of the printing and typesetting 
+						</p>
+					</div>
+				</div>
+			</div>		
+		</div>
+		<div class="container-fluid fifth">
+			<div class="row">
+				<div class="col-md-3 container3">	
+  					<img src="<?php bloginfo('template_url'); ?>/images/images (1).jpg" alt="Avatar" width="337px" height="515px" class="nimg image3">
+  					<div class="middle">
+    					<div class="text">Amazing Location</div>
+  					</div>
+  				</div>
+  				<div class="col-md-6 container3">	
+  					<img src="<?php bloginfo('template_url'); ?>/images/pexels-photo-1396122.jpeg" alt="Avatar" width="680px" height="515px" class="nimg image3">
+  					<div class="middle">
+    					<div class="text">Amazing Designs</div>
+  					</div>
+  				</div>
+  				<div class="col-md-3 container3">	
+  					<img src="<?php bloginfo('template_url'); ?>/images/pexels-photo-1080696.jpeg" alt="Avatar" width="336px" height="515px" class="nimg image3">
+  					<div class="middle">
+    					<div class="text">Delight Life</div>
+  					</div>
+  				</div>
+  			 </div>
+		</div>
+		<div class="container ">
+			<div class="row">
+				<div class="col-md-3 ">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px"> 
+				</div>
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>		
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>		
+				<div class="col-md-3">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo2 (2).png" height="250px" width="250px">
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="col-md-3">
+				<p>Phone:37787358758</p>
+				<p>Mail:hello@gmail.com</p>
+				<p>Address:Livingstone,Brooklyne,NY 11201</p>
+			</div>
+		</div>
+<?php get_footer(); ?>
